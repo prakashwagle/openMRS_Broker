158b1044f90dec2a463c2f61565bf103a7c4ceb7
Prakash Wagle <prakashwagle@live.in>
2015-05-02 17:43:02 -0400

    Second Commit - Working without UUID

--- src/main/java/edu/uncc/hspms/service/PatientService.java
+++ src/main/java/edu/uncc/hspms/service/PatientService.java
@@ -76,33 +76,37 @@ public class PatientService {
 	 //	 int encounter_id=0;
 	 //    int obs_id=0;
 	 //    int visit_id=0;
+		String uuid="9e91c11d-877a-4b20-a851-c7ea0cda5da7";
 		Visit visit=new Visit();
 	     Encoutnter encounter= new Encoutnter();
 	     Observation observation = new Observation();
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 		java.sql.Connection c = jdbcTemplate.getDataSource().getConnection();
-		String sql="Select max(visit_id) + 1 as visit_id from openmrs.visit;";
-		String sql1="Select max(encounter_id) + 1 as encounter_id from openmrs.encounter;";
-		String sql2="Select max(obs_id) + 1 as obs_id from openmrs.obs;";
+		String sql="Select max(visit_id) + 1 as visit_id from openmrs.visit";
+		String sql1="Select max(encounter_id) + 1 as encounter_id from openmrs.encounter";
+		String sql2="Select max(obs_id) + 1 as obs_id from openmrs.obs";
 		Statement stmt = c.createStatement();
 		ResultSet rset = stmt.executeQuery(sql);
+		rset.first();
 		visit.setVisit_id(rset.getInt("visit_id"));
 		rset = stmt.executeQuery(sql1);
+		rset.first();
 		encounter.setEncounter_id(rset.getInt("encounter_id"));
 		rset = stmt.executeQuery(sql2);
+		rset.first();
 		observation.setObs_id(rset.getInt("obs_id"));
 		
 		Patient patient = jdbcTemplate.queryForObject(sql,new BeanPropertyRowMapper<Patient>(Patient.class));
 		String sql_visit = "INSERT INTO openmrs.visit (visit_id, patient_id, visit_type_id, date_started, date_stopped, location_id, creator, date_created, changed_by, date_changed, voided, uuid) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
-		int count_visit = jdbcTemplate.update(sql_visit,visit.getVisit_id(),patient_id,visit.getVisit_type_id(),visit.getDate_started(),visit.getDate_stopped(),visit.getLocation_id(),visit.getCreator(),visit.getDate_created(),visit.getChanged_by(),visit.getDate_changed(),visit.getVoided(),visit.getUuid());
+		int count_visit = jdbcTemplate.update(sql_visit,visit.getVisit_id(),patient_id,visit.getVisit_type_id(),visit.getDate_started(),visit.getDate_stopped(),visit.getLocation_id(),visit.getCreator(),visit.getDate_created(),visit.getChanged_by(),visit.getDate_changed(),visit.getVoided(),uuid);
 		System.out.println("Visit count=" + count_visit);
 		
 		String sql_encounter="INSERT INTO openmrs.encounter (encounter_id, encounter_type, patient_id, location_id, form_id, encounter_datetime, creator, date_created, voided, visit_id, uuid) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
-		int count_encounter= jdbcTemplate.update(sql_encounter,encounter.getEncounter_id(),encounter.getEncounter_type(),patient_id,encounter.getLocation_id(),encounter.getForm_id(),encounter.getEncounter_datetime(),encounter.getCreator(),encounter.getVoided(),visit.getVisit_id(),encounter.getUuid());
+		int count_encounter= jdbcTemplate.update(sql_encounter,encounter.getEncounter_id(),encounter.getEncounter_type(),patient_id,encounter.getLocation_id(),encounter.getForm_id(),encounter.getEncounter_datetime(),encounter.getCreator(),encounter.getDate_created(),encounter.getVoided(),visit.getVisit_id(),uuid);
 		System.out.println("Encounter count"+ count_encounter);
 		
 		String sql_observation="INSERT INTO openmrs.obs (obs_id, person_id, concept_id, encounter_id, obs_datetime, location_id, value_text, creator, date_created) VALUES (?,?,?,?,?,?,?,?,?);";
-		int count_observation=jdbcTemplate.update(sql_observation, observation.getObs_id(),patient,observation.getConcept_id(),observation.getEncounter_id(),observation.getObs_datetime(),observation.getLocation_id(),chartReview,observation.getCreator(),observation.getDate_created());
+		int count_observation=jdbcTemplate.update(sql_observation, observation.getObs_id(),patient_id,observation.getConcept_id(),encounter.getEncounter_id(),observation.getObs_datetime(),observation.getLocation_id(),chartReview,observation.getCreator(),observation.getDate_created());
 		System.out.println("Observation count"+ count_observation);
 		System.out.println("Chart Review Sucessfully created for PatientID : "+patient_id);
 	}
